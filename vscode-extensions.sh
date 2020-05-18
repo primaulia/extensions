a3cadef11791b732d64a293ca5e22624fc9ad7a1
Prima Aulia Gusta <primAulia@gmail.com>
2020-05-18 13:24:10 +0800

    Update vscode-extensions.sh

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,19 +1,9 @@
-pkglist=(
-CoenraadS.bracket-pair-colorizer-2
-dbaeumer.vscode-eslint
-esbenp.prettier-vscode
-misogi.ruby-rubocop
-ms-vscode.sublime-keybindings
-ms-vsliveshare.vsliveshare
-ms-vsliveshare.vsliveshare-audio
-ms-vsliveshare.vsliveshare-pack
-PKief.material-icon-theme
-rayhanw.erb-helpers
-xyz.local-history
-Atishay-Jain.All-Autocomplete
-eamodio.gitlens
-)
+#!/bin/bash
+exho_info "Installing VSCode"
+_install code
 
-for i in ${pkglist[@]}; do
-  code --install-extension $i
-done
+while read EXTENSION
+do
+    echo_info "Install VSCode Extension: $EXTENSION"
+    code --install-extension $EXTENSION
+done < $HOME/.dotfiles/vscode/extensions.txt 
